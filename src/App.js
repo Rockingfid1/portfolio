7bcc4e958dba4579fcab708930763d385a034d37
rockingfid1 <chilliwillizpeeps@gmail.com>
2024-12-27 14:49:15 -0500

    added loading page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import ModalContextProvider from "./store/modal-context";
 import SectionOne from "./components/SectionOne";
 import SectionTwo from "./components/SectionTwo";
 import SectionThree from "./components/SectionThree";
 import Footer from "./components/Footer";
+import LoadingPage from "./components/LoadingPage";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  window.addEventListener("load", () => {
+    setLoading(false);
+    console.log("page loaded");
+  });
+
   return (
     <ModalContextProvider>
-      <SectionOne />
-      <SectionThree />
-      <SectionTwo />
-      <Footer />
+      {loading && <LoadingPage />}
+      {!loading && (
+        <>
+          <SectionOne />
+          <SectionThree />
+          <SectionTwo />
+          <Footer />
+        </>
+      )}
     </ModalContextProvider>
   );
 }
---
+++ src/components/LoadingPage.js
@@ -0,0 +1,14 @@
+import LoadingIndicator from "./UI/Loading";
+
+export default function LoadingPage() {
+  return (
+    <div className="w-screen h-screen bg-slate-500 place-content-center">
+      <div className="flex flex-col items-center w-fit h-fit p-9 m-auto mt-auto">
+        <LoadingIndicator />
+        <h1 className="text-3xl font-bold text-white lp:text-5xl 2md:text-4xl 2xl:text-6xl xl:text-5xl lg:text-5xl">
+          Page Loading...
+        </h1>
+      </div>
+    </div>
+  );
+}
